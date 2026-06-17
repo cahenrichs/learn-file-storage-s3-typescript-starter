dcf538c48bf1ed5f88b2bee8b1e5ce46bae22a5e
Clint <cahenrichs@gmail.com>
2026-06-17 11:46:01 -0500

    added three handlers in assets.ts

--- src/api/assets.ts
+++ src/api/assets.ts
@@ -1,9 +1,30 @@
 import { existsSync, mkdirSync } from "fs";
 
 import type { ApiConfig } from "../config";
+import path from "path";
 
 export function ensureAssetsDir(cfg: ApiConfig) {
   if (!existsSync(cfg.assetsRoot)) {
     mkdirSync(cfg.assetsRoot, { recursive: true });
   }
+}
+
+export function mediaTypeToExt(mediaType: string) {
+  const split = mediaType.split("/")
+  if (split.length !== 2) {
+    return ".bin"
+  }
+  return `.${split[1]}`
+}
+
+export function getAssetDiskPath(cfg: ApiConfig, assetPath: string) {
+  return path.join(
+    cfg.assetsRoot,
+    assetPath,
+  )
+}
+
+export function getAssetURL(cfg: ApiConfig, assetPath: string) {
+  const newUrl = `http://localhost:${cfg.port}/assets/${assetPath}`
+  return newUrl
 }
